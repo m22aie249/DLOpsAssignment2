3b4a476b6d63714210ff97dacfa1b5b36579807b
m22aie249 <m22aie249@iitj.ac.in>
2024-03-31 14:03:51 +0530

    creating bug in data

--- Activity1_2b.py
+++ Activity1_2b.py
@@ -9,7 +9,7 @@ def leaky_relu(x, alpha=0.01):
 def tanh(x):
     return np.tanh(x)
 
-random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
+random_values = [3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
 
 print("ReLU values for random data:")
 for value in random_values:
