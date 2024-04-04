0ce6e8537af0abb21670df1f315eae38b00d31b8
m22aie249 <m22aie249@iitj.ac.in>
2024-04-04 12:34:35 +0530

    fix random value data

--- Activity1_2.py
+++ Activity1_2.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-random_values = [3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
+random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
